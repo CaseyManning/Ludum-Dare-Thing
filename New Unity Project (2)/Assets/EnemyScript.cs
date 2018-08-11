d94faeaacf64b3b4ab861e39c2e45ca7c31f1519
CaseyManning <kcgrind@gmail.com>
2018-08-10 21:40:56 -0700

    randomly spawning enemies

--- New Unity Project (2)/Assets/EnemyScript.cs
+++ New Unity Project (2)/Assets/EnemyScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour {
 
-	float speed = 5;
+	float speed = 2;
 
 	bool alive = true;
 
---
+++ New Unity Project (2)/Assets/EnemySpawning.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawning : MonoBehaviour {
+
+	public GameObject basicEnemy;
+
+	float enemyCooldown = 3;
+	float enemyCooldownTimer = 3;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		enemyCooldownTimer -= Time.deltaTime;
+
+		if(enemyCooldownTimer < 0) {
+			enemyCooldownTimer = enemyCooldown;
+			GameObject enemy = Instantiate(basicEnemy);
+			enemy.transform.position = new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), 0);
+
+			
+		}
+	}
+}
