a92786c13743d9d45966055b240fafaae170e096
CaseyManning <kcgrind@gmail.com>
2018-08-10 20:10:50 -0700

    partially fixed movement/collision code

--- New Unity Project (2)/Assets/PlayerMove.cs
+++ New Unity Project (2)/Assets/PlayerMove.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour {
 
-	float speed = 10;
+	float speed = 100;
 
 	float shotCooldown = 0.3f;
 
@@ -12,9 +12,11 @@ public class PlayerMove : MonoBehaviour {
 
 	public GameObject bullet;
 
+	Rigidbody2D rigid;
+
 	// Use this for initialization
 	void Start () {
-		
+		rigid = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
@@ -24,12 +26,26 @@ public class PlayerMove : MonoBehaviour {
 
 		float xSpeed = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
 		float ySpeed = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-		transform.Translate(xSpeed, ySpeed, 0);
+		// transform.Translate(xSpeed, ySpeed, 0);
+		// rigid.velocity = new Vector2(xSpeed, ySpeed);
+		// rigid.AddForce(new Vector2(xSpeed, ySpeed));
 
 		if(Input.GetKey("space") && shotCooldownTimer < 0) {
 			GameObject bul = Instantiate(bullet);
 			shotCooldownTimer = shotCooldown;
 			bullet.transform.position = transform.position;
 		}
+	}
+	private void FixedUpdate() {
+		// float xSpeed = Input.GetAxis("Horizontal") * speed * 30 * Time.deltaTime;
+		// float ySpeed = Input.GetAxis("Vertical") * speed * 30 * Time.deltaTime;
+		// rigid.AddForce(new Vector2(xSpeed, ySpeed));
+
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		Vector2 movement = new Vector2(moveHorizontal * speed, moveVertical * speed);
+		rigid.AddForce(movement);
+		rigid.velocity = Vector3.ClampMagnitude(rigid.velocity, 3);
+
 	}
 }
